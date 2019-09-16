8f576e7727a744d60a4d88ffb74d6bece449be33
System Administrator <root@Rajendra-mbp.local>
2019-09-16 13:23:16 +0700

    Added modification

--- taboola_selector.js
+++ taboola_selector.js
@@ -12,14 +12,22 @@ function updateBanner(event) {
   if (!!!resultBanner)
     return false;
 
+    /**
+     * No need right now the popup, since added comment.
+     */
   // combine the node name, classes, and id into a string in the banner
-  var resultContent = '<p>You\'re hovering on = { ';
-  resultContent += 'node: <b> ' + node + '</b>; ';
-  resultContent += 'classes: <b> ' + classList + '</b>; ';
-  resultContent += 'id: <b> ' + id + '</b>;';
-  resultContent +=  ' }</p>';
+  // var resultContent = '<p>You\'re hovering on = { ';
+  // resultContent += 'node: <b> ' + node + '</b>; ';
+  // resultContent += 'classes: <b> ' + classList + '</b>; ';
+  // resultContent += 'id: <b> ' + id + '</b>;';
+  // resultContent +=  ' }</p>';
 
-  resultBanner.innerHTML = resultContent;
+  // resultBanner.innerHTML = resultContent;
+
+  console.log('Node: ', node);
+  console.log('\nClasses: ', classList);
+  console.log('\nId: ', id);
+  console.log('\nxpath: ', event);
 }
 
 // toggles the visibility of the banner
