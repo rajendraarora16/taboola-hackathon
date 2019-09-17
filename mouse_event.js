43af74c4217d3a7f95c5af353236189d799ac18f
System Administrator <root@Rajendra-mbp.local>
2019-09-17 14:28:28 +0700

    Added modification changes

--- mouse_event.js
+++ mouse_event.js
@@ -1,14 +1,5 @@
 /**
  * Disable mouse click
  */
-// document.oncontextmenu=RightMouseDown;
-// function RightMouseDown() { return false;}
-
-/**
- * Add click listener event
- */
-document.addEventListener('contextmenu', function(event) {
-    event.preventDefault();
-    console.log('enalbles')
-    updateSidebarMenu(event);
- }, true); 
+document.oncontextmenu=RightMouseDown;
+function RightMouseDown() { return false;}
